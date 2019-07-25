ef7b0172d289b2113c7f607d71c7a57182d38309
Kostyantyn Drobyazko <kostyantyn_drobyazko@epam.com>
2019-07-25 12:04:44 +0300

    Initial commit

---
+++ bus-routes-service/src/main/java/com/goeuro/challenges/busroutesservice/BusRoutesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.goeuro.challenges.busroutesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusRoutesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusRoutesServiceApplication.class, args);
+	}
+
+}
